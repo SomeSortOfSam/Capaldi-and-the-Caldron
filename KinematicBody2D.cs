11cfb15bdeb63ce47057ff600ba94d555da484fd
MeepeLuke <57550626+MeepeLuke@users.noreply.github.com>
2021-08-09 12:48:59 -0500

    Platforming

---
+++ KinematicBody2D.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public class KinematicBody2D : Godot.KinematicBody2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	[Export] public Vector2 gravity;
+	private Vector2 velocity;
+	[Export] public float speed;
+
+
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(float delta)
+	{
+		velocity += movementHandeler() * speed;
+		velocity += gravity;
+		velocity = MoveAndSlide(velocity);
+	}
+	public Vector2 movementHandeler()
+	{
+		Vector2 movementVector = Vector2.Zero;
+		if (Input.IsActionPressed("ui_left"))
+		{
+			movementVector += Vector2.Left;
+		}
+		if (Input.IsActionPressed("ui_right"))
+		{
+			movementVector += Vector2.Right;
+		}
+		if (Input.IsActionPressed("ui_up"))
+		{
+			movementVector += Vector2.Up;
+		}
+		if (Input.IsActionPressed("ui_down"))
+		{
+			movementVector += Vector2.Down;
+		}
+		return movementVector.Normalized();
+	}
+}
