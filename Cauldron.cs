5b2aa0452886a686304a4f39ca48bf266d457655
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-08-09 11:28:07 -0500

    IFood

--- Cauldron.cs
+++ Cauldron.cs
@@ -3,19 +3,37 @@ using System;
 
 public class Cauldron : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private IFood[] ingredients = new IFood[3];
+    private int ingredientIndex = 0;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public bool tryingRecipie = false;
+
+    [Signal]
+    public delegate void TryRecipe(object result);
+
+    public void AddFood(IFood newFood)
     {
-        
+        ingredientIndex++;
+        ingredients[ingredientIndex] = newFood;
+        if(ingredientIndex >= ingredients.Length - 1)
+        {
+        }
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void CheckRecipe()
+    {
+        tryingRecipie = true;
+        GD.Print(ingredients[0].ToString() + "," + ingredients[1].ToString() + "," + ingredients[2].ToString());
+    }
+
+    private void _on_FoodZone_body_entered(object body)
+    {
+        if(body is IFood)
+        {
+            AddFood(body as IFood);
+        }
+    }
 }
+
+
+
