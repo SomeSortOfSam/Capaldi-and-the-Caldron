d99e8650c955412fc1e204a1c26f96533ef130a1
MeepeLuke <57550626+MeepeLuke@users.noreply.github.com>
2021-08-09 16:41:42 -0500

    Git is hard
    sorry about that

--- Player.gd
+++ Player.gd
@@ -1,16 +1,28 @@
 extends KinematicBody2D
 
+var speed
+var velocity = Vector2.ZERO;
+export var gravity: Vector2 = Vector2.ZERO;
+export var weight: Vector2
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _physics_process(delta):
+	
+	velocity += get_horizontal_input_vector()
+	velocity += gravity;
+	
+	
+	velocity = move_and_slide(velocity)
+	
+	
+func get_horizontal_input_vector()->Vector2:
+	var movementVector:Vector2
+	if(Input.is_action_just_pressed("ui_left")):
+		movementVector += Vector2.LEFT
+	if(Input.is_action_just_pressed("ui_right")):
+		movementVector += Vector2.RIGHT
+	return movementVector.normalized()
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_gravity()->Vector2:
+	var local_gravity = Input.gravity*weight
+	
+	return local_gravity;
