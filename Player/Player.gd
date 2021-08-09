2be073fbf7cd7f1047e47e25b2a3587a0be36c56
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-08-09 17:32:33 -0500

    Animated Player

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,28 +1,55 @@
 extends KinematicBody2D
 
-var speed
-var velocity = Vector2.ZERO;
-export var gravity: Vector2 = Vector2.ZERO;
-export var weight: Vector2
 
-func _physics_process(delta):
-	
-	velocity += get_horizontal_input_vector()
-	velocity += gravity;
-	
+const ANIM_JUMP_PREFIX = "Jump"
+const ANIM_IDEL_PREFIX = "Still"
+const ANIM_CROUCH_PREFIX = "Crouch"
+const ANIM_WALK_PREFIX = "Walk"
+
+const ANIM_HOLDING_SUFFIX = "Hold"
+const ANIM_NOT_HOLDING_SUFFIX = "Empty"
+
+export var SPEED : float
+export var GRAVITY: Vector2;
+export var FRICTION : float
+export var holding : bool
+
+onready var sprite : AnimatedSprite = $Icon
+
+var velocity := Vector2.ZERO;
+
+func _process(_delta):
+	sprite.flip_h = velocity.x > 0
+	sprite.animation = get_animation_name()
+
+func _physics_process(_delta):
+	velocity += get_horizontal_input_vector() * SPEED
+	velocity += GRAVITY;
 	
 	velocity = move_and_slide(velocity)
-	
-	
+	velocity.x *= FRICTION
+
 func get_horizontal_input_vector()->Vector2:
-	var movementVector:Vector2
-	if(Input.is_action_just_pressed("ui_left")):
+	var movementVector := Vector2.ZERO
+	if(Input.is_action_pressed("ui_left")):
 		movementVector += Vector2.LEFT
-	if(Input.is_action_just_pressed("ui_right")):
+	if(Input.is_action_pressed("ui_right")):
 		movementVector += Vector2.RIGHT
 	return movementVector.normalized()
 
-func get_gravity()->Vector2:
-	var local_gravity = Input.gravity*weight
-	
-	return local_gravity;
+func get_animation_name() -> String:
+	var name := ""
+	name += get_animation_prefix() 
+	name += "_" 
+	name += get_animation_suffix()
+	return name
+
+func get_animation_suffix() -> String:
+	return ANIM_HOLDING_SUFFIX if holding else ANIM_NOT_HOLDING_SUFFIX
+
+func get_animation_prefix() -> String:
+	if velocity.y > 0:
+		return ANIM_JUMP_PREFIX
+	if !Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
+		return ANIM_IDEL_PREFIX
+	return ANIM_WALK_PREFIX
