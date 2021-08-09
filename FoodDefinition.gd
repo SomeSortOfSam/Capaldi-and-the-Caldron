b881f81802442fce9a20341e651b006ab777c477
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-08-09 14:51:08 -0500

    Food

---
+++ FoodDefinition.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name FoodDefinition
+
+export var sprite : Texture
---
+++ Recipe.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Recipe
+
+export var food1 : Resource
+export var food2 : Resource
+export var food3 : Resource
