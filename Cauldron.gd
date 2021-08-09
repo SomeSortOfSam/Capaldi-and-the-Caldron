d1f306e7c2cc1dd11656b602bab2cbf5617fd1a0
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-08-09 16:16:35 -0500

    Recipe Tests

--- Cauldron.gd
+++ Cauldron.gd
@@ -1,19 +1,27 @@
 extends Node2D
 class_name Cauldron
 
-var ingredinets := [3]
+export var recipes := []
+
+var ingredinets := []
 var ingredinets_index := 0
 
-signal checked_recipe()
+signal checked_recipe(is_valid)
 
 func add_food(food : Food):
-	ingredinets.append(food)
+	ingredinets.append(food.definition)
 	if ingredinets.size() >= 3:
 		check_recipe()
 
 func check_recipe():
+	for recipe in recipes:
+		if recipe is Recipe:
+			if ingredinets[0] == recipe.food1 && ingredinets[1] == recipe.food2 && ingredinets[2] == recipe.food3:
+				emit_signal("checked_recipe", true)
+				ingredinets.clear()
+				return
 	ingredinets.clear()
-	emit_signal("checked_recipe")
+	emit_signal("checked_recipe", false)
 
 func _on_FoodZone_body_entered(body):
 	if body is Food:
